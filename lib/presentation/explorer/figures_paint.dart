9f7860683494c9e00177d31fc61afb36ea5f4897
Andreskiu <andreskiu@hotmail.com>
2022-05-22 18:58:11 -0300

    Fill object detected with colors

--- lib/presentation/explorer/figures_paint.dart
+++ lib/presentation/explorer/figures_paint.dart
@@ -21,37 +21,58 @@ class FiguresPainter extends CustomPainter {
       var r = (_random.nextDouble() * 255).floor();
       var g = (_random.nextDouble() * 255).floor();
       var b = (_random.nextDouble() * 255).floor();
-      final _randomColor = Color.fromRGBO(r, g, b, 1);
-      final _paint = Paint()
-        ..color = _randomColor
-        ..strokeWidth = 3;
+      final _randomColor = Color.fromRGBO(r, g, b, 0.7);
+
+      final _paint = Paint()..color = _randomColor;
+
       final _scaleFactorHorizontal = size.width / originalSize.width;
       final _scaleFactorVertical = size.height / originalSize.height;
 
-      // canvas.translate(
-      //   figure.initialPoint.x.toDouble() * _scaleFactorHorizontal,
-      //   figure.initialPoint.y.toDouble() * _scaleFactorVertical,
-      // );
+      // ======== GENERATE COLORFULL PATH ========
+      final _path = Path();
+      final _points = [
+        Offset(figure.initialPoint.x.toDouble() * _scaleFactorHorizontal,
+            figure.initialPoint.y.toDouble() * _scaleFactorVertical)
+      ];
+      _points.addAll(figure.points.map(
+        (point) => Offset(point.x.toDouble() * _scaleFactorHorizontal,
+            point.y.toDouble() * _scaleFactorVertical),
+      ));
+      _path.addPolygon(_points, true);
+      canvas.drawPath(_path, _paint);
 
+      // ======== GENERATE BLACK PERIMETER LINE ========
+      final _paint2 = Paint()
+        ..color = Colors.black
+        ..strokeWidth = 3;
       Point _lastPoint = figure.initialPoint;
+
       figure.points.forEach((point) {
         canvas.drawLine(
-          Offset(_lastPoint.x.toDouble() * _scaleFactorHorizontal,
-              _lastPoint.y.toDouble() * _scaleFactorVertical),
-          Offset(point.x.toDouble() * _scaleFactorHorizontal,
-              point.y.toDouble() * _scaleFactorVertical),
-          _paint,
+          Offset(
+            _lastPoint.x.toDouble() * _scaleFactorHorizontal,
+            _lastPoint.y.toDouble() * _scaleFactorVertical,
+          ),
+          Offset(
+            point.x.toDouble() * _scaleFactorHorizontal,
+            point.y.toDouble() * _scaleFactorVertical,
+          ),
+          _paint2,
         );
         _lastPoint = point;
       });
 
-      // close de path
+      // close de line
       canvas.drawLine(
-        Offset(_lastPoint.x.toDouble() * _scaleFactorHorizontal,
-            _lastPoint.y.toDouble() * _scaleFactorVertical),
-        Offset(figure.initialPoint.x.toDouble() * _scaleFactorHorizontal,
-            figure.initialPoint.y.toDouble() * _scaleFactorVertical),
-        _paint,
+        Offset(
+          _lastPoint.x.toDouble() * _scaleFactorHorizontal,
+          _lastPoint.y.toDouble() * _scaleFactorVertical,
+        ),
+        Offset(
+          figure.initialPoint.x.toDouble() * _scaleFactorHorizontal,
+          figure.initialPoint.y.toDouble() * _scaleFactorVertical,
+        ),
+        _paint2,
       );
     });
   }
